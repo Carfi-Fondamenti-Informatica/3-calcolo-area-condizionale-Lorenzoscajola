31ac9342851e52235771abda8e2d1e5d37d1722c
Lorenzoscajola <117931214+Lorenzoscajola@users.noreply.github.com>
2022-11-19 15:52:12 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
-using namespace std;
+using namespace std; 
 
-int main() {
-   return 0;
+int main () {
+	float a;
+	float b;
+	int c=0; 
+	
+    
+   cin>> a;
+	cin>> b; 
+	cin>> c;
+	
+	switch(c) {
+		case 0:
+			cout<<((b*a)/2)<<endl;
+			break; 
+	    case 1:
+	    	cout<<(a*a)<<endl;
+	    	break;
+	    case 2:
+	    	cout<<(a*b)<<endl;
+	    	break;
+	    	
+	}
+	
+	return 0;
 }
